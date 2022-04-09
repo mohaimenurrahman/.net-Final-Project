8cc4228b0e291fe59e43ad1c834445ad44ea9722
mohaimenurrahman <mohaimenur888@gmail.com>
2022-04-09 15:55:16 +0600

    cRUD/Flats

---
+++ BillMan/BLL/FlatService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using BLL.BEnt;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class FlatService
+    {
+        public static List<FlatModel> Get()
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<building, BuildingModel>();
+                c.CreateMap<flat, FlatModel>();
+            });
+            var mapper = new Mapper(config);
+            var data = mapper.Map<List<FlatModel>>(flatRepo.Get());
+            return data;
+
+        }
+
+        public static List<string> GetNames()
+        {
+            var data = flatRepo.Get().Select(e => e.flatRenterName).ToList();
+            return data;
+        }
+
+        public static void Add(FlatModel s)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<FlatModel, flat>();
+
+            });
+            var mapper = new Mapper(config);
+            var data = mapper.Map<flat>(s);
+            flatRepo.Add(data);
+        }
+    }
+}
---
+++ BillMan/BillMan/Controllers/FlatController.cs
@@ -0,0 +1,33 @@
+﻿using BLL;
+using BLL.BEnt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BillMan.Controllers
+{
+    public class FlatController : ApiController
+    {
+        [Route("api/Flat/FlatRenterNames")]
+        [HttpGet]
+        public List<string> GetNames()
+        {
+            return FlatService.GetNames();
+        }
+        [Route("api/Flat/All")]
+        [HttpGet]
+        public List<FlatModel> GetAll()
+        {
+            return FlatService.Get();
+        }
+        [Route("api/Flat/Create")]
+        [HttpPost]
+        public void Add(FlatModel s)
+        {
+            FlatService.Add(s);
+        }
+    }
+}
